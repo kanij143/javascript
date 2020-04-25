9233db83f5375888d8164b7eb40e443577cae65f
Kanij <kanij.cse43@gmail.com>
2020-04-26 00:35:23 +0600

    Completed practice 2

---
+++ class_02.js
@@ -0,0 +1,14 @@
+
+ // another way
+ var a=12, b=13, c=5, d=9;
+ console.log(a,b,c,d);
+/*data type
+String("abcd","kanij")
+Number(-12,1,.2)
+Boolean(true,false)
+undefined
+null
+*/
+var n=.2;
+console.log(typeof n);
+console.log(n);
