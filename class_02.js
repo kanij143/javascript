5b97b7524b8da51f41adf0bac506d81933098152
Kanij <kanij.cse43@gmail.com>
2020-04-26 00:38:32 +0600

    code formated

--- class_02.js
+++ class_02.js
@@ -1,7 +1,8 @@
 
  // another way
- var a=12, b=13, c=5, d=9;
- console.log(a,b,c,d);
+ var a = 12,  b = 13, c = 5, d = 9;
+
+ console.log(a, b, c, d);
 /*data type
 String("abcd","kanij")
 Number(-12,1,.2)
@@ -9,6 +10,7 @@ Boolean(true,false)
 undefined
 null
 */
-var n=.2;
+var n = .2;
+
 console.log(typeof n);
 console.log(n);
