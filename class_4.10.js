9ac758dc5aed139c95d0c3ee6dc220c1b56abe51
Kanij <kanij.cse43@gmail.com>
2020-04-27 01:42:58 +0600

    Completed practice 4.5

---
+++ class_4.10.js
@@ -0,0 +1,19 @@
+// ternary operator
+
+var n = 2;
+var result;
+var reminder = n % 2;
+
+result = (0 == reminder) ? "even" : "odd";
+
+console.log("this number is", result);
+
+
+// negative positive cheeck using ternary operator
+
+var n = -6;
+var result;
+
+result = (n < 0) ? "negative" : "positive";
+
+console.log("this number is",result);
