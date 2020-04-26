cab04f7294d7311b5a0e8cb10bc1e3f0087df101
Kanij <kanij.cse43@gmail.com>
2020-04-26 21:10:11 +0600

    code modified

--- class_03.js
+++ class_03.js
@@ -1,5 +1,7 @@
 //cost of eggs calculate
-var egg_price=5;
-var number_of_eggs=12;
-var total_cost_of_eggs= egg_price*number_of_eggs;
-console.log("total cost is",total_cost_of_eggs,"taka");
+
+var egg_price = 5;
+var number_of_eggs = 12;
+var total_cost_of_eggs = egg_price*number_of_eggs;
+
+console.log("total cost is", total_cost_of_eggs, "taka");
