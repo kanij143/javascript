663c2cb31d5f6145af9c62fad653ce899591d44c
Kanij <kanij.cse43@gmail.com>
2020-04-26 21:55:21 +0600

    Completed practice 5

--- class_05.js
+++ class_05.js
@@ -1,8 +1,9 @@
 //prime numbr cheeck
-/*var n =8;
+
+/*var n = 8;
 
 if(n<10){
-    if(2==n || 3==n ||5==n ||7==n){
+    if(2 == n || 3 == n ||5 == n ||7 == n){
         console.log("this number",n,"is smaller than 10 and its prime numbr");
 
     }else{
@@ -15,10 +16,12 @@ if(n<10){
     */
 
     // another way
-    var n=7;
-    if(n<10 && (2==n || 3==n ||5==n ||7==n)){
+
+    var n = 7;
+
+    if(n < 10 && (2 == n || 3 == n || 5 == n ||7 == n)){
         console.log("this number",n,"is smaller than 10 and its prime numbr");
-    } else if (n< 10){
+    } else if (n < 10){
         console.log("this number",n,"is smaller than 10 and its  not prime numbr");
 
     } else{
