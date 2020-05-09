b6ed3effce43676720e638f7e40d2d69181503b3
Kanij <kanij.cse43@gmail.com>
2020-05-09 22:39:06 +0600

    Completed Practice 6.1

---
+++ class_6.1.js
@@ -0,0 +1,15 @@
+ //  Introduction of Aarry
+ 
+ var fruits =["apple","orange","mango","banana"];
+
+ console.log(fruits[0]);
+
+ fruits[4] = "grape";
+
+ console.log(fruits[4]);
+
+ console.log(fruits.length);
+
+ var last_index = fruits.length -1;
+
+ console.log(fruits[last_index]);
